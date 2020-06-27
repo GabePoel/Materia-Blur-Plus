d57020058311d8b59c6bda0e9896ac975dde0751
Gabriel Perko-Engel <gpe@berkeley.edu>
2020-06-27 08:04:22 -0700

    Allowed for global install.

--- install.py
+++ install.py
@@ -2,25 +2,42 @@ import os
 import sys
 
 home = os.path.expanduser("~")
+usr = os.path.expanduser("/usr")
 insdir = os.getcwd()
-tardir = os.path.join(home, ".local", "share", "plasma", "desktoptheme")
+locdir = os.path.join(home, ".local", "share", "plasma", "desktoptheme")
+usrdir = os.path.join(usr, "share", "plasma", "desktoptheme")
 
-def link(name):
-    #if not os.path.exists(os.path.join(tardir, name)):
-        #os.makedirs(os.path.join(tardir, name))
-    os.system("ln -sf '" + os.path.join(insdir, name) + "' " + tardir)
+atroot = False
 
-def move(name):
-    os.system("mvdir '" + os.path.join(insdir, name) + "' " + tardir)
+def link(name, root=False):
+    prefix = ""
+    tardir = locdir
+    if root:
+        prefix = "sudo "
+        tardir = usrdir
+    os.system(prefix + "ln -sf '" + os.path.join(insdir, name) + "' " + tardir)
+
+def move(name, root=False):
+    prefix = ""
+    tardir = locdir
+    if root:
+        prefix = "sudo "
+        tardir = usrdir
+    os.system(prefix + "mvdir '" + os.path.join(insdir, name) + "' " + tardir)
     
 themes = ["Materia-Blur-Plus"]
 
 if len(sys.argv) == 1:
     raise ValueError("No input provided.\nPlease specify either 'link' or 'move'.")
+try:
+    if sys.argv[2] == "-root":
+        atroot = True
+except:
+    pass
 for theme in themes:
     if sys.argv[1] == "link":
-        link(theme)
+        link(theme, atroot)
     elif sys.argv[1] == "move":
-        move(theme)
+        move(theme, atroot)
     else:
         raise ValueError("Invalid input provided.\nPlease specify either 'link' or 'move'.")
